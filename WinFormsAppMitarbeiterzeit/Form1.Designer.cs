61bb21ef16ea820a1a657fe5cf6427f1d2f18866
Dominik Aust <AustDominik@student.dv-schulen.de>
2025-11-10 20:12:20 +0100

    Projektdateien hinzufügen.

---
+++ WinFormsAppMitarbeiterzeit/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WinFormsAppMitarbeiterzeit
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBoxMitarbeiterzeiten = new ListBox();
+            comboBoxProjekte = new ComboBox();
+            buttonAuswerten = new Button();
+            labelAnzahl = new Label();
+            textBoxAuswerten = new TextBox();
+            textBoxDurchschnitt = new TextBox();
+            buttonDurchschnitt = new Button();
+            SuspendLayout();
+            // 
+            // listBoxMitarbeiterzeiten
+            // 
+            listBoxMitarbeiterzeiten.FormattingEnabled = true;
+            listBoxMitarbeiterzeiten.ItemHeight = 25;
+            listBoxMitarbeiterzeiten.Location = new Point(51, 55);
+            listBoxMitarbeiterzeiten.Name = "listBoxMitarbeiterzeiten";
+            listBoxMitarbeiterzeiten.Size = new Size(609, 204);
+            listBoxMitarbeiterzeiten.TabIndex = 0;
+            // 
+            // comboBoxProjekte
+            // 
+            comboBoxProjekte.FormattingEnabled = true;
+            comboBoxProjekte.Location = new Point(52, 290);
+            comboBoxProjekte.Name = "comboBoxProjekte";
+            comboBoxProjekte.Size = new Size(260, 33);
+            comboBoxProjekte.TabIndex = 1;
+            // 
+            // buttonAuswerten
+            // 
+            buttonAuswerten.Location = new Point(336, 288);
+            buttonAuswerten.Name = "buttonAuswerten";
+            buttonAuswerten.Size = new Size(145, 34);
+            buttonAuswerten.TabIndex = 2;
+            buttonAuswerten.Text = "Auswerten";
+            buttonAuswerten.UseVisualStyleBackColor = true;
+            buttonAuswerten.Click += buttonAuswerten_Click;
+            // 
+            // labelAnzahl
+            // 
+            labelAnzahl.AutoSize = true;
+            labelAnzahl.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelAnzahl.Location = new Point(706, 65);
+            labelAnzahl.Name = "labelAnzahl";
+            labelAnzahl.Size = new Size(126, 48);
+            labelAnzahl.TabIndex = 3;
+            labelAnzahl.Text = "Anzahl";
+            // 
+            // textBoxAuswerten
+            // 
+            textBoxAuswerten.Location = new Point(514, 295);
+            textBoxAuswerten.Name = "textBoxAuswerten";
+            textBoxAuswerten.Size = new Size(150, 31);
+            textBoxAuswerten.TabIndex = 4;
+            // 
+            // textBoxDurchschnitt
+            // 
+            textBoxDurchschnitt.Location = new Point(514, 347);
+            textBoxDurchschnitt.Name = "textBoxDurchschnitt";
+            textBoxDurchschnitt.Size = new Size(150, 31);
+            textBoxDurchschnitt.TabIndex = 6;
+            // 
+            // buttonDurchschnitt
+            // 
+            buttonDurchschnitt.Location = new Point(336, 340);
+            buttonDurchschnitt.Name = "buttonDurchschnitt";
+            buttonDurchschnitt.Size = new Size(145, 34);
+            buttonDurchschnitt.TabIndex = 5;
+            buttonDurchschnitt.Text = "Durchschnitt";
+            buttonDurchschnitt.UseVisualStyleBackColor = true;
+            buttonDurchschnitt.Click += buttonDurchschnitt_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(860, 414);
+            Controls.Add(textBoxDurchschnitt);
+            Controls.Add(buttonDurchschnitt);
+            Controls.Add(textBoxAuswerten);
+            Controls.Add(labelAnzahl);
+            Controls.Add(buttonAuswerten);
+            Controls.Add(comboBoxProjekte);
+            Controls.Add(listBoxMitarbeiterzeiten);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBoxMitarbeiterzeiten;
+        private ComboBox comboBoxProjekte;
+        private Button buttonAuswerten;
+        private Label labelAnzahl;
+        private TextBox textBoxAuswerten;
+        private TextBox textBoxDurchschnitt;
+        private Button buttonDurchschnitt;
+    }
+}
---
+++ WinFormsAppMitarbeiterzeit/Mitarbeiterzeit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsAppMitarbeiterzeit
+{
+    public class Mitarbeiterzeit
+    {
+        private string mitarbeitername;
+        private DateTime datum;
+        private double stunden;
+        private string projekt;
+
+        public Mitarbeiterzeit(string mitarbeitername, DateTime datum, double stunden, string projekt)
+        {
+            this.mitarbeitername = mitarbeitername;
+            this.datum = datum;
+            this.stunden = stunden;
+            this.projekt = projekt;
+        }
+
+        public string Mitarbeitername { get => mitarbeitername; set => mitarbeitername = value; }
+        public DateTime Datum { get => datum; set => datum = value; }
+        public double Stunden { get => stunden; set => stunden = value; }
+        public string Projekt { get => projekt; set => projekt = value; }
+
+
+    }
+}
