2f714890824c2bb45c05770dc126551f09b10452
Domi2445 <austdominik@student.dv-schulen.de>
2025-12-09 13:51:18 +0100

    Delegate Einführung upload

---
+++ WindowsFormsAppDelegateEinführung/WindowsFormsAppDelegateEinführung/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppDelegateEinführung
+{
+    public partial class Form1 : Form
+    {
+        Vorschlaege vorschlaege;
+        public Form1()
+        {
+            InitializeComponent();
+            vorschlaege = new Vorschlaege();
+            aktualisieren();
+        }
+
+        private void buttonAufnehmen_Click(object sender, EventArgs e)
+        {
+            vorschlaege.VorschlagAufnehmen(textBoxVorschlag.Text);
+            vorschlaege.OnVorschlaegeDoppeltDelegate += Doppeltwarnung;
+            vorschlaege.OnVorschlaegeDelegate += Anzahlerreicht;
+            aktualisieren();
+        }
+
+        private void Anzahlerreicht()
+        {
+            MessageBox.Show("Es können nur 3 Vorschläge hinzugefügt werden", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void aktualisieren()
+        {
+            listBoxVorschlag.DataSource = null;
+            listBoxVorschlag.DataSource = vorschlaege.Liste;
+        }
+
+        private void buttonAlleLoeschen_Click(object sender, EventArgs e)
+        {
+            vorschlaege.AlleLoeschen();
+            aktualisieren();
+        }
+
+        private void Doppeltwarnung()
+        {
+            MessageBox.Show("Der Vorschlag ist schon Vorhanden", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
---
+++ WindowsFormsAppDelegateEinführung/WindowsFormsAppDelegateEinführung/Vorschlaege.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppDelegateEinführung
+{
+    class Vorschlaege
+    {
+        List<string> liste;
+        public delegate void VorschlaegeDelegate();
+        public VorschlaegeDelegate OnVorschlaegeDelegate = null;
+        public delegate void VorschlaegeDoppeltDelegate();
+        public VorschlaegeDoppeltDelegate OnVorschlaegeDoppeltDelegate = null;
+
+        public List<string> Liste { get => liste; private set => liste = value; }
+
+        public Vorschlaege()
+        {
+            liste = new List<string>();
+        }
+        public void VorschlagAufnehmen(string Vorschlag)
+        {
+            if (liste.Count != 3)
+            {
+                liste.Add(Vorschlag);
+
+                foreach(String s in liste)
+                {
+                    if(s.Equals(Vorschlag))
+                    {
+                        OnVorschlaegeDoppeltDelegate?.Invoke();
+                    }
+                }
+
+                
+            }
+            else
+            {
+                OnVorschlaegeDelegate?.Invoke();
+            }
+        }
+        public void AlleLoeschen()
+        {
+            liste.Clear();
+        }
+    }
+}
