5fb2632cf4508469cab1ae4ba4b08d02639ba52b
Domi2445 <austdominik@student.dv-schulen.de>
2025-12-09 13:50:25 +0100

    Delegate Vorschläge upload

---
+++ VORLAGE_Einfuehrung_Vorschlaege/WindowsFormsAppDelegateEinführung/Vorschlaege.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppDelegateEinführung
+{
+    class Vorschlaege
+    {
+        List<string> liste;
+        public delegate void VorschlaegeDelegate();
+        public VorschlaegeDelegate OnVorschlaegeDelegate = null;
+        public delegate void VorschlaegeDoppeltDelegate();
+        public VorschlaegeDoppeltDelegate OnVorschlaegeDoppeltDelegate = null;
+
+        public List<string> Liste { get => liste; private set => liste = value; }
+
+        public Vorschlaege()
+        {
+            liste = new List<string>();
+        }
+        public void VorschlagAufnehmen(string Vorschlag)
+        {
+            // Prüfen ob Liste voll ist
+            if (liste.Count >= 3)
+            {
+                OnVorschlaegeDelegate?.Invoke();
+                
+            }
+
+            // Prüfen ob Vorschlag schon existiert
+            if (liste.Contains(Vorschlag))
+            {
+                OnVorschlaegeDoppeltDelegate?.Invoke();
+                
+            }
+
+            else
+            {
+                // Vorschlag hinzufügen
+                liste.Add(Vorschlag);
+            }
+        }
+        public void AlleLoeschen()
+        {
+            liste.Clear();
+        }
+    }
+}
