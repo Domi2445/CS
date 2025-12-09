19987cfef11131aa4e1a3aff75a19d57c51780df
Domi2445 <austdominik@student.dv-schulen.de>
2025-12-09 13:52:53 +0100

    Vererbung einstieg upload

---
+++ ConsoleAppVererbung/ConsoleAppVererbung/Hund.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppVererbung
+{
+    internal class Hund : Tier
+    {
+        public string Rasse { get; set; }
+        public Hund(string name, string rasse) : base(name)
+        {
+            Rasse = rasse;
+        }
+
+        public override void Essen()
+        {
+            Console.WriteLine($"{Name} frisst sein Hundefutter.");
+        }
+        public void Bellen()
+        {
+            Console.WriteLine($"{Name} bellt: Wuff!");
+        }
+    }
+}
---
+++ ConsoleAppVererbung/ConsoleAppVererbung/Tier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppVererbung
+{
+    internal class Tier
+    {
+        public string Name { get; set; }
+
+        public Tier(string name)
+        {
+            Name = name;
+            Console.WriteLine($"Tierkonstruktor mit Parameter aufgerufen: {Name}");
+        }
+
+        public virtual void Essen()
+        {
+            Console.WriteLine($"Name {Name} isst gerade");
+        }
+    }
+}
